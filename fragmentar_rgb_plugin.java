2f74f121e0df9661207ea981e090248dd0f7a055
Lucas Rocha <rochadefreitaslucas@gmail.com>
2021-06-13 13:39:49 -0300

    fragmentar imagem rgb aplicando lut

--- fragmentar_rgb_plugin.java
+++ fragmentar_rgb_plugin.java
@@ -2,40 +2,35 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.PlugIn;
 import ij.process.ImageProcessor;
+import ij.process.LUT;
 	 
-public class fragmentar_rgb_plugin implements PlugIn {
+public class Fragmentar_Rgb_Plugin implements PlugIn {
 	
-	public int validar(int aux) {
-		if(aux >255) {
-			return 255;
+
+	public byte[] generateLutComValores() {
+		byte[] aux = new byte[256];
+		for (int i = 0; i < 256; i++) {
+			aux[i] = (byte) i;
 		}
-		if(aux < 0) {
-			return 0;
+		return aux;
+	}
+	public byte[] generateLutComZero() {
+		byte[] aux = new byte[256];
+		for (int i = 0; i < 256; i++) {
+			aux[i] = 0;
 		}
-		return  aux;
-	}
-	public int avg(int[] pixel) {
-		int aux = (pixel[0] + pixel[1] + pixel[2])/3; 
-		return validar(aux);
-	}
-	
-	public int lum1(int[] pixel) {
-		int aux = (int) (( 0.299 * pixel[0] ) + ( 0.587 * pixel[1] ) + ( 0.114 * pixel[2] )) ;
-		return validar(aux);
-	}
-	public int lum2(int[] pixel) {
-		int aux = (int) ( (pixel[0] * 0.2125) + (pixel[1] * 0.7154) + (pixel[2] * 0.072)) ;
-		return validar(aux);
+		return aux;
 	}
 	
 	public void run(String arg) {
 		ImagePlus imagem = IJ.getImage();
 		ImageProcessor processador = imagem.getProcessor();
 		int pixel[]  = new int[3];
-	
+		byte[] lut = generateLutComValores();
+		byte[] lutZero = generateLutComZero();
 		
-		ImagePlus read = IJ.createImage("RED", "8-bit", processador.getWidth(), processador.getHeight(), 1);
-		ImageProcessor procRed = read.getProcessor();
+		ImagePlus red = IJ.createImage("RED", "8-bit", processador.getWidth(), processador.getHeight(), 1);
+		ImageProcessor procRed = red.getProcessor();
 		ImagePlus green = IJ.createImage("GREEN", "8-bit", processador.getWidth(), processador.getHeight(), 1);
 		ImageProcessor procGreen = green.getProcessor();
 		ImagePlus blue = IJ.createImage("BLUE", "8-bit", processador.getWidth(), processador.getHeight(), 1);
@@ -49,14 +44,21 @@ public class fragmentar_rgb_plugin implements PlugIn {
 				procBlue.putPixel(i, j, pixel[2]);
 			}
 		}
-		read.show();
+		
+		LUT lutRed = new LUT(lut, lutZero, lutZero);
+		LUT lutGreen = new LUT(lutZero, lut, lutZero);
+		LUT lutBlue = new LUT(lutZero, lutZero, lut);
+		
+		red.setLut(lutRed);
+		green.setLut(lutGreen);
+		blue.setLut(lutBlue);
+		
+		red.show();
 		green.show();
 		blue.show();
+		
+		
+		
+		
 	}
 }
-
-
-
-
-
-
